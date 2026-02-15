7129c9275909cea2000cf359f8e23c31f0df9479
Kun-fy <kosakatakun@gmail.com>
2026-02-15 18:08:20 +0700

    Create setup.py

---
+++ setup.py
@@ -0,0 +1,16 @@
+import os
+
+print("""[0] pip\n[1] pip3\nWhich one do you use?""")
+
+c = input(">>>: ")
+if c == "0":
+    os.system("pip install pysocks")
+    os.system("apt-get install python2")
+    os.system("apt-get install python3")
+    os.system("pip install slowloris")
+elif c == "1":
+    os.system("pip3 install pysocks")
+    os.system("apt-get install python2")
+    os.system("apt-get install python3")
+    os.system("pip install slowloris")
+print("Done.")
